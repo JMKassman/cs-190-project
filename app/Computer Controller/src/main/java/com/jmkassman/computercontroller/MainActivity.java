0dc3d78128923f68fe6b34a0f99246b22063eedf
Josh Kassman <jkassman2@gmail.com>
2018-03-22 19:11:04 -0400

    started app

--- app/Computer Controller/src/main/java/com/jmkassman/computercontroller/MainActivity.java
+++ app/Computer Controller/src/main/java/com/jmkassman/computercontroller/MainActivity.java
@@ -2,12 +2,40 @@ package com.jmkassman.computercontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "Main Activity";
+
+    @BindView(R.id.edit_text)
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
