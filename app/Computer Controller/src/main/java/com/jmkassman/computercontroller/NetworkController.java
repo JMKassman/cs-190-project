0e0fe3f92107c3529bce36d2da4d1cc776f4f1a5
Josh Kassman <jkassman2@gmail.com>
2018-04-06 16:04:42 -0400

    trying networking

---
+++ app/Computer Controller/src/main/java/com/jmkassman/computercontroller/NetworkController.java
@@ -0,0 +1,16 @@
+package com.jmkassman.computercontroller;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class NetworkController extends AsyncTask<String, Void, Void> {
+    //https://developer.android.com/reference/android/os/AsyncTask.html
+    @Override
+    protected Void doInBackground(String... strings) {
+        return null;
+    }
+}
