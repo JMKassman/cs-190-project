a612280510d00c4b0ac2a30bd5b6e79b8667bada
Josh Kassman <jkassman2@gmail.com>
2018-04-16 01:00:58 -0400

    bug fixes

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,61 +1,81 @@
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.io.*;
-import java.net.*;
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.util.map;
+import static java.awt.event.KeyEvent.*;
 
 public class Server {
-    public static void main(String[] args) throws IOException, AWTException, InterruptedException {
+    private static Socket socket;
+    private static ObjectInputStream OIS;
+
+    public static void main(String[] args) throws IOException, AWTException {
         //just adding something to start off with
         ServerSocket serverSocket = new ServerSocket(45342);
         System.out.println("created server socket " + serverSocket.toString());
         Robot robot = new Robot();
-        Map<String, String> keyCodes = Map.of(" ","VK_SPACE","BACKSPACE","VK_BACK","0","0x30","1","0x31", "2","0x32","3","0x33","4","0x34", "5","0x35", "6","0x36", "7", "0x37");
-        keyCodes.put("8","0x38");
-        keyCodes.put("9","0x39");
-        keyCodes.put("A","0x42");
-        keyCodes.put("B","0x42");
-        keyCodes.put("C","0x43");
-        keyCodes.put("D","0x44");
-        keyCodes.put("E","0x45");
-        keyCodes.put("F","0x46");
-        keyCodes.put("G","0x47");
-        keyCodes.put("H","0x48");
-        keyCodes.put("I","0x49");
-        keyCodes.put("J","0x4A");
-        keyCodes.put("K","0x4B");
-        keyCodes.put("L","0x4C");
-        keyCodes.put("M","0x4D");
-        keyCodes.put("N","0x4E");
-        keyCodes.put("O","0x4F");
-        keyCodes.put("P","0x50");
-        keyCodes.put("Q","0x51");
-        keyCodes.put("R","0x52");
-        keyCodes.put("S","0x53");
-        keyCodes.put("T","0x54");
-        keyCodes.put("U","0x55");
-        keyCodes.put("V","0x56");
-        keyCodes.put("X","0x58");
-        keyCodes.put("Y","0x59");
-        keyCodes.put("Z","0x5A");
+        Map<String, Integer> keyCodes = new HashMap<>();
+        keyCodes.put(" ", VK_SPACE);
+        keyCodes.put("BACKSPACE", VK_BACK_SPACE);
+        keyCodes.put("0",VK_0);
+        keyCodes.put("1",VK_1);
+        keyCodes.put("2",VK_2);
+        keyCodes.put("3",VK_3);
+        keyCodes.put("4",VK_4);
+        keyCodes.put("5",VK_5);
+        keyCodes.put("6",VK_6);
+        keyCodes.put("7",VK_7);
+        keyCodes.put("8",VK_8);
+        keyCodes.put("9",VK_9);
+        keyCodes.put("A",VK_A);
+        keyCodes.put("B",VK_B);
+        keyCodes.put("C",VK_C);
+        keyCodes.put("D",VK_D);
+        keyCodes.put("E",VK_E);
+        keyCodes.put("F",VK_F);
+        keyCodes.put("G",VK_G);
+        keyCodes.put("H",VK_H);
+        keyCodes.put("I",VK_I);
+        keyCodes.put("J",VK_J);
+        keyCodes.put("K",VK_K);
+        keyCodes.put("L",VK_L);
+        keyCodes.put("M",VK_M);
+        keyCodes.put("N",VK_N);
+        keyCodes.put("O",VK_O);
+        keyCodes.put("P",VK_P);
+        keyCodes.put("Q",VK_Q);
+        keyCodes.put("R",VK_R);
+        keyCodes.put("S",VK_S);
+        keyCodes.put("T",VK_T);
+        keyCodes.put("U",VK_U);
+        keyCodes.put("V",VK_V);
+        keyCodes.put("X",VK_X);
+        keyCodes.put("Y",VK_Y);
+        keyCodes.put("Z",VK_Z);
         try {
-            Socket socket = serverSocket.accept();
-            ObjectInputStream OIS = new ObjectInputStream(socket.getInputStream());
             System.out.println("created socket and ois");
             while (true) {
-                String readable = "Hello World";
+                socket = serverSocket.accept();
+                OIS = new ObjectInputStream(socket.getInputStream());
+                String readable;
                 System.out.println("waiting for object on " + socket.getInetAddress().toString() + " on port " + socket.getPort());
                 readable = (String) OIS.readObject();
-                if (keyCodes.containsValue(readable)){
-                    robot.keyPress(keyCodes.get(readable));
+                System.out.println(readable);
+                if (keyCodes.containsKey(readable.toUpperCase())){
+                    System.out.println("pressing key " + keyCodes.get(readable.toUpperCase()));
+                    robot.keyPress(keyCodes.get(readable.toUpperCase()));
+                    Thread.sleep(100);
+                    robot.keyRelease(keyCodes.get(readable.toUpperCase()));
                 }
             }
         } catch (IOException e){
-            
+            e.printStackTrace();
         } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
