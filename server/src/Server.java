e10d35b9db840068c3b1de9a2122550912f69501
Josh Kassman <jkassman2@gmail.com>
2018-04-15 21:39:40 -0400

    base app done

--- server/src/Server.java
+++ server/src/Server.java
@@ -4,14 +4,17 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    public static void main(String[] args) throws IOException, ClassNotFoundException{
+    public static void main(String[] args) throws IOException {
         //just adding something to start off with
-        ServerSocket serverSocket = new ServerSocket(1234);
+        ServerSocket serverSocket = new ServerSocket(45342);
+        System.out.println("created server socket " + serverSocket.toString());
         try {
             Socket socket = serverSocket.accept();
             ObjectInputStream OIS = new ObjectInputStream(socket.getInputStream());
+            System.out.println("created socket and ois");
             while (true) {
                 String readable = "Hello World";
+                System.out.println("waiting for object on " + socket.getInetAddress().toString() + " on port " + socket.getPort());
                 readable = (String) OIS.readObject();
                 System.out.println(readable);
             }
